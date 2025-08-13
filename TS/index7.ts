443a691ab4cb0a4b0b3360039f4ac24b5f255ad0
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-08-13 17:13:28 -0300

    att

--- TS/index7.ts
+++ TS/index7.ts
@@ -5,3 +5,17 @@
 
    // const[contador, setContador] = useState<number>(0);
 //}
+
+let cor: String = "preto";
+
+switch(cor) {
+    case "vermelho":
+        console.log("A cor é vermelha");
+        break;
+    case "branco":
+        console.log("A cor é branco");
+        break;
+    case "preto":
+        console.log("A cor é preto");
+        break;
+}
